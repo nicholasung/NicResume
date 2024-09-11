85e4e752397b2a10a75592d43b8de3784cad3e03
nicung <q5y7r@students.cs.ubc.ca>
2024-09-11 15:50:12 -0700

    quick saveing

--- nic-resume/src/app/page.tsx
+++ nic-resume/src/app/page.tsx
@@ -2,13 +2,12 @@ import Edu from "../app/components/edu";
 import Exp from "../app/components/exp";
 import Footer from "../app/components/footer";
 import Header from "../app/components/header";
-import Navbar from "../app/components/navbar";
 import Projects from "../app/components/projects";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      {/* <Navbar /> */}
       <Header />
       <div>
         <p>intro blurb</p>
