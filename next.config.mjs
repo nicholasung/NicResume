f9fddcf503992f4b2ea8df202ab75f04fb3ffd39
nicholasung <nicholas.ung0@gmail.com>
2025-01-27 20:38:00 -0800

    build and local server now works correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true, // Disable the Image Optimization API
-  },
 };
 
 export default nextConfig;
