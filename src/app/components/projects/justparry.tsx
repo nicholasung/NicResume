475aacf96fc12771909efbab3526aac11fc7dd4e
nicholasung <nicholas.ung0@gmail.com>
2025-03-01 11:40:45 -0800

    fixed misstyled font

--- src/app/components/projects/justparry.tsx
+++ src/app/components/projects/justparry.tsx
@@ -21,8 +21,8 @@ export const Jp: React.FC<JPProps> = () => {
                 Just Parry
             </h3> 
             <b>C++, OpenGL, Git</b><br/>
-            <p style={{fontSize: '14px'}}>September 2024 - December 2024</p>
-            <p style={{ maxWidth: '400px', textAlign: 'center' }}>
+            <p className={styles.text}>September 2024 - December 2024</p>
+            <p className={styles.text}>
                 A fighting game prototype built with a custom engine in C++ with a focus on responsiveness. My significant contributions includes leading scrums, low level polling logic, code reviews and documentation. Built with guidance from UBC Course CPSC 427: Video Game Programming.
             </p>
         </div>
