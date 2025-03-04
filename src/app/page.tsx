758fc34d99964013dfae50c711e6fe1b2b80989c
nicholasung <nicholas.ung0@gmail.com>
2025-03-04 15:44:09 -0800

    new intro blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       {/* <Navbar /> */}
       <Header />
       <main>
-        <div className='intro'>Welcome to my website! I&apos;m Nic, a Computer Science major at UBC. I have been surrounded by computers all my life and have been expounding on my interest since I was a kid. In my free time I enjoy Cars, Weightlifting, and Video Games</div>
+        <div className='intro'>Welcome to my website! I&apos;m Nic, a Computer Science major at UBC. I am interested in Embedded Microcontroller Programming, HomeLab Self Hosting, and Product Development. In the past I have sold products focused on gamecube controller analog filtering and input reading. Currently I am looking to diversify my skill set with web development. At the same time developing telemetry and monitoring devices for cars as a hobby. In my free time I enjoy Cars, Weightlifting, and Video Games</div>
         <Projects />
         <Exp />
         <EdCerts/>
